e4768dc9dbf70a68a4b841d3da0f376ae6e9a1e6
Patrick F. <patrick.frueh@gmx.net>
2025-03-18 16:34:13 +0100

    Use 'go fmt' formatter

--- learn_the_basics/declarations.go
+++ learn_the_basics/declarations.go
@@ -1,15 +1,16 @@
 package main
+
 import "fmt"
 
 func main() {
-  // Type is specified using keyword "string"
-  var student1 string = "John"
+	// Type is specified using keyword "string"
+	var student1 string = "John"
 
-  // Type is inferred
-  var student2 = "Jane"
-  x := 2
+	// Type is inferred
+	var student2 = "Jane"
+	x := 2
 
-  fmt.Println(student1)
-  fmt.Println(student2)
-  fmt.Println(x)
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
 }
